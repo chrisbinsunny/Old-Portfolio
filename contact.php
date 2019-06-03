0a54625073735c174db6263dce7ec8d494bee3ce
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2019-06-03 13:43:02 +0530

    Added more Skills

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@
 $from = 'Demo contact form <demo@domain.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <bikas9971@gmail.com>';
+$sendTo = 'chrisbinofficial@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
